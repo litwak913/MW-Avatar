75c903646eb3c1e0979cd743686359fca21aecae
litwak913 <litwak913@qq.com>
2023-04-26 23:07:22 +0800

    style

--- example/Gadget-ShowAvatar.js
+++ example/Gadget-ShowAvatar.js
@@ -1,3 +1,3 @@
-var img = $('<img>').attr('src', mw.config.get('wgScriptPath') + '/extensions/Avatar/avatar.php?user=' + mw.user.id());
-var link = $('<a>').attr('href', mw.util.getUrl('Special:UploadAvatar')).append(img);
-$('#pt-userpage').before($('<li id="pt-avatar"></li>').append(link));
+const $img = $( '<img>' ).attr( 'src', mw.config.get( 'wgScriptPath' ) + '/extensions/Avatar/avatar.php?user=' + mw.user.id() );
+const $link = $( '<a>' ).attr( 'href', mw.util.getUrl( 'Special:UploadAvatar' ) ).append( $img );
+$( '#pt-userpage' ).before( $( '<li>' ).attr( 'id', 'pt-avatar' ).append( $link ) );
--- example/Gadget-UserLinkAvatar.js
+++ example/Gadget-UserLinkAvatar.js
@@ -1,4 +1,4 @@
-$('.mw-userlink').each(function(_, item) {
-	item = $(item);
-	item.prepend($('<img/>').addClass('userlink-avatar').attr('src', mw.config.get('wgScriptPath') + '/extensions/Avatar/avatar.php?user=' + item.text()));
-});
+$( '.mw-userlink' ).each( function ( _, $item ) {
+	const $useritem = $( $item );
+	$useritem.prepend( $( '<img>' ).addClass( 'userlink-avatar' ).attr( 'src', mw.config.get( 'wgScriptPath' ) + '/extensions/Avatar/avatar.php?user=' + $useritem.text() ) );
+} );
